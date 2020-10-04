c1b5751e2341e89111d4ffedfaa11181c7aa2ba2
olmer88 <zhuk.maxim.a@gmail.com>
2020-10-04 13:24:07 +0300

    intervals

--- intervals.py
+++ intervals.py
@@ -1,5 +1,5 @@
 def is_in_range(n):
-    return -15 <= n <= 12 or 14 <= n <= 17 or 19 <= n
+    return -15 < n <= 12 or 14 < n < 17 or 19 <= n
 
 
 if __name__ == '__main__':
@@ -7,14 +7,13 @@ if __name__ == '__main__':
 
 
 def test_is_in_range():
-    assert not is_in_range(-16)
-    assert is_in_range(-15)
+    assert not is_in_range(-15)
     assert is_in_range(3)
     assert is_in_range(12)
-    assert not is_in_range(13)
-    assert is_in_range(14)
+    assert not is_in_range(14)
     assert is_in_range(15)
-    assert is_in_range(17)
+    assert is_in_range(16)
+    assert not is_in_range(17)
     assert not is_in_range(18)
     assert is_in_range(19)
     assert is_in_range(1000000)
